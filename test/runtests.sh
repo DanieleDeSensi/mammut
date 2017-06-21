73f914fa97f46ff635befd5d1d51f0c9370fa283
Daniele De Sensi <d.desensi.software@gmail.com>
2017-06-21 11:42:38 +0200

    [IMP] Testing.

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-cd archs 
-if [ ! -d "repara" ]; then
-	tar -xvf repara.tar.gz && rm -f repara.tar.gz
-fi
-cd ..
 for t in *.cpp; do
-	echo $t
+	cd archs && tar -xf repara.tar.gz
+	cd .. 
 	./$(basename "$t" .cpp)
+    exitvalue=$?
+	cd archs && rm -rf repara # Remove folder after test since it becomes dirty
+	cd ..
+    if [ $exitvalue -ne 0 ]; then
+        break
+    fi
 done
+
+exit $exitvalue
