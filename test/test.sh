8e94341c452255b2984352d29b3aa85528d04ba4
Daniele De Sensi <d.desensi.software@gmail.com>
2019-09-19 18:50:30 +0200

    [FIX] Minor.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd archs || exit
 tar -xf repara.tar.gz
+tar -xf c8.tar.gz
 cd ..
 $1
 rm -rf archs/repara # Remove folder after test since it becomes dirty
+rm -rf archs/c8 # Remove folder after test since it becomes dirty
